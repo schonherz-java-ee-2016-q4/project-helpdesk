e77e7a21cfedf4782e29db5649cdbbd5bbaa0754
David Pinter <djazz90@gmail.com>
2017-01-24 19:05:14 +0100

    Started jpa annotation config.

---
+++ helpdesk-data/src/main/java/hu/schonherz/training/helpdesk/data/config/HelpDeskDataModuleConfig.java
@@ -0,0 +1,10 @@
+package hu.schonherz.training.helpdesk.data.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("hu.schonherz.training.helpdesk.data")
+public class HelpDeskDataModuleConfig {
+    
+}
