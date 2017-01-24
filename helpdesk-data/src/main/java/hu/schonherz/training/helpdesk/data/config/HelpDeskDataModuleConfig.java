0326f992772367d07f72873cf99d0b19a50b708c
Pintér Dávid <djazz90@gmail.com>
2017-01-24 20:21:30 +0100

    Now creates entities in database.

--- helpdesk-data/src/main/java/hu/schonherz/training/helpdesk/data/config/HelpDeskDataModuleConfig.java
+++ helpdesk-data/src/main/java/hu/schonherz/training/helpdesk/data/config/HelpDeskDataModuleConfig.java
@@ -1,10 +1,4 @@
 package hu.schonherz.training.helpdesk.data.config;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("hu.schonherz.training.helpdesk.data")
 public class HelpDeskDataModuleConfig {
-    
 }
---
+++ helpdesk-data/src/main/java/hu/schonherz/training/helpdesk/data/entity/TestEntity.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.schonherz.training.helpdesk.data.entity;
+
+import javax.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class TestEntity extends BaseEntity {
+
+    private static final long serialVersionUID = 1L;
+
+    private String content;
+}
