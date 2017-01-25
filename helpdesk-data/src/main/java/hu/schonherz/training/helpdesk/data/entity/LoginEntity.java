0469155b2a6d7084dac17fa5a2f2f0f38844e1c9
dratech <dratech92@gmail.com>
2017-01-25 14:12:20 +0100

    Changed loginDate type to LocalDateTime.

--- helpdesk-data/src/main/java/hu/schonherz/training/helpdesk/data/entity/LoginEntity.java
+++ helpdesk-data/src/main/java/hu/schonherz/training/helpdesk/data/entity/LoginEntity.java
@@ -1,11 +1,10 @@
 package hu.schonherz.training.helpdesk.data.entity;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -18,7 +17,6 @@ public class LoginEntity extends BaseEntity {
     private static final long serialVersionUID = -6201828861327999500L;
     private int agentId;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date loginDate;
+    private LocalDateTime loginDate;
 
 }
