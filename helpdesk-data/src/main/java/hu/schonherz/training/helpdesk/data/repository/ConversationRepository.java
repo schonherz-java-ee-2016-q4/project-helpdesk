66e568b90e117eebbb8ae62c7d93d1dd7db1a506
marcellnemeth <marcellnemeth20@gmail.com>
2017-01-25 11:42:59 +0100

    Update ConversationRepository.java

--- helpdesk-data/src/main/java/hu/schonherz/training/helpdesk/data/repository/ConversationRepository.java
+++ helpdesk-data/src/main/java/hu/schonherz/training/helpdesk/data/repository/ConversationRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-interface ConversationRepository extends JpaRepository<ConversationEntity, Integer> {
+public interface ConversationRepository extends JpaRepository<ConversationEntity, Integer> {
 
 }
