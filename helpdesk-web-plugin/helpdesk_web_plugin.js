995fdf02ae0c38d5164bce1f313f57864c6fd0f3
aron9609 <aron9609@gmail.com>
2017-01-28 17:47:42 +0100

    Added navigation to chat page

--- helpdesk-web-plugin/helpdesk_web_plugin.js
+++ helpdesk-web-plugin/helpdesk_web_plugin.js
@@ -1,14 +1,13 @@
 var $div = $('<div />').appendTo('body');
 var $text_$div = $('<div />').appendTo($div);
-var $btn = $('<button/>').appendTo($div);
+var $btn = $('<a/>').appendTo($div);
 
 $(document).ready(function () {
     $btn.append("Yes");
     $text_$div.attr('class', 'helpdesk_plugin_text')
     $div.attr('class', 'helpdesk_plugin');
-    $btn.attr('class', 'helpdesk_plugin_button');
-    $btn.attr('src', 'https://javatraining.neuron.hu/helpdesk/chat.xhtml');
-    $text_$div.append("Can we help you? ");
+    $btn.attr('class', 'helpdesk_plugin_link');
+    $btn.attr('href', 'http://javatraining.neuron.hu/helpdesk/secured/chat.xhtml');
+    $text_$div.append("Can we help you?");
     $div.append($btn);
-
 });
