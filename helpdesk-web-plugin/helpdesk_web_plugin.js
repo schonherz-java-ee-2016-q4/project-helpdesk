2a69052f66f6d11eeb3f205ce8add205a6c65124
aron9609 <aron9609@gmail.com>
2017-01-29 18:53:17 +0100

    #12 The async activity tracking works batter, with the added error handling.

--- helpdesk-web-plugin/helpdesk_web_plugin.js
+++ helpdesk-web-plugin/helpdesk_web_plugin.js
@@ -1,22 +1,30 @@
-var $div = $('<div />').appendTo('body');
-var $text_$div = $('<div />').appendTo($div);
-var $btn = $('<a/>').appendTo($div);
+var $pluginLayout = $('<div />').appendTo('body');
+var $textParent = $('<div />').appendTo($pluginLayout);
+var $pluginMainText = $('<div />').appendTo($textParent);
+var $pluginLink = $('<a/>').appendTo($textParent);
+var $errorMessage = $('<div />').appendTo($pluginLayout);
 
 var uuid;
 
 $(document).ready(function () {
+    $pluginLayout.attr('class', 'helpdesk_plugin');
+    $textParent.attr('class', 'helpdesk_plugin_text_parent')
+    $pluginMainText.attr('class', 'helpdesk_plugin_text');
+    $pluginLink.attr('class', 'helpdesk_plugin_link');
+    $errorMessage.attr('class', 'helpdesk_error_text');
 
-    $btn.append("Yes");
-    $text_$div.attr('class', 'helpdesk_plugin_text')
-    $div.attr('class', 'helpdesk_plugin');
-    $btn.attr('class', 'helpdesk_plugin_link');
-    $text_$div.append("Can we help you?");
-    $div.append($btn);
+    $pluginMainText.append("If you have any problem with this site");
+    $pluginLink.append("Click here");
+
 
     generateUUID();
 
-    $btn.on('click', function () {
+    $pluginLink.on('click', function () {
         getAvailableAgent();
+    });
+
+    $("form").on('submit', function () {
+        onFormSubmit(this);
     });
 
     $("a").on('click', function () {
@@ -31,6 +39,8 @@ $(document).ready(function () {
         onButtonClicked(this);
     });
 
+    $errorMessage.hide()
+
 });
 
 function generateUUID() {
@@ -40,10 +50,35 @@ function generateUUID() {
     });
 }
 
+function onFormSubmit(element) {
+    var target;
+
+    if ($(element).attr('action') != undefined) {
+        target = $(element).attr('action');
+    }
+    else if ($(element).attr('id') != undefined) {
+        target = $(element).attr('id');
+    }
+    else if ($(element).attr('name') != undefined) {
+        target = $(element).attr('name');
+    }
+    else {
+        target = ($(element)[0]).toString();
+    }
+    postActivity(uuid, "FORM_SUBMIT", target);
+}
+
 function onAnchorClicked(element) {
     var target;
+
     if ($(element).attr('href') != undefined) {
         target = $(element).attr('href');
+    }
+    else if ($(element).attr('id') != undefined) {
+        target = $(element).attr('id');
+    }
+    else if ($(element).attr('name') != undefined) {
+        target = $(element).attr('name');
     }
     else {
         target = ($(element)[0]).toString();
@@ -60,7 +95,7 @@ function onButtonClicked(element) {
     if ($(element).text() != "") {
         target = $(element).text();
     }
-    if ($(element).attr('id') != undefined) {
+    else if ($(element).attr('id') != undefined) {
         target = $(element).attr('id');
     }
     else if ($(element).attr('name') != undefined) {
@@ -71,15 +106,15 @@ function onButtonClicked(element) {
     }
 
     postActivity(uuid, "BUTTON_CLICK", target);
-    if (element.attr('id') != undefined) {
-        target = element.attr('id');
-    }
-    else {
-        target = element.attr('id');
-    }
-    doPost(uuid, "BUTTON_CLICK", target);
 }
 
+function displayError(message) {
+
+    $errorMessage.html(message);
+    $errorMessage.slideDown("fast").delay(5000);
+    $errorMessage.slideUp("slow");
+
+}
 
 function postActivity(uuid, type, target) {
 
@@ -91,7 +126,7 @@ function postActivity(uuid, type, target) {
     $.ajax({
         type: "POST",
         contentType: "application/json",
-        url: "javatraining.neuron.hu/helpdesk/api/activites/",
+        url: "http://javatraining.neuron.hu/helpdesk/api/activites/",
         data: JSON.stringify(form),
         dataType: 'json',
         timeout: 100000,
@@ -100,28 +135,33 @@ function postActivity(uuid, type, target) {
 
 function getAvailableAgent() {
 
+    $pluginLink.fadeOut("fast");
+
     var form = {};
-    form["source"] = window.location.href;
-    console.log(window.location.href);
+    form["source"] = window.location.host;
 
     $.ajax({
         type: "GET",
         contentType: "application/json",
-        url: "javatraining.neuron.hu/helpdesk/api/isagentavailable",
+        url: "http://javatraining.neuron.hu/helpdesk/api/isagenavailable",
         data: JSON.stringify(form),
         dataType: 'json',
         timeout: 100000,
         success: function (response) {
+            console.log(response);
             if (response.agentId == null) {
-                //TODO: Display an error message
+                displayError("There isn't any available agent!");
             }
             else {
-                var chatPage = window.open('http://javatraining.neuron.hu/helpdesk/secured/chat/' + agentId);
+                var chatPage = window.open('http://javatraining.neuron.hu/helpdesk/secured/chat' + agentId);
                 chatPage.focus();
             }
         },
         error: function (e) {
-            //TODO: Display an error message
+            displayError("We couldn't connect to the helpdesk site");
+        },
+        complete: function () {
+            $pluginLink.fadeIn("fast");
         }
     });
 }
