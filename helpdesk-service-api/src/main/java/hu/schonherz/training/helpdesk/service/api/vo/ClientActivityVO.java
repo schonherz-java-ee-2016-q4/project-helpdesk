08549d2ab1edd092ec884a4b21453d9052971724
David Pinter <djazz90@gmail.com>
2017-02-01 04:04:06 +0100

    #15 Jackson now uses timestamps. ClientActivityVO changed to support Jackson. Smaller changes in some files.

--- helpdesk-service-api/src/main/java/hu/schonherz/training/helpdesk/service/api/vo/ClientActivityVO.java
+++ helpdesk-service-api/src/main/java/hu/schonherz/training/helpdesk/service/api/vo/ClientActivityVO.java
@@ -1,19 +1,51 @@
 package hu.schonherz.training.helpdesk.service.api.vo;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @Data
 public class ClientActivityVO {
+    //Ignored some of the getters and setters from lombok code generation,
+    //because Jackson couldn't ignore some fields from deserialization otherwise.
     @JsonIgnore
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
     private long id;
+
     private String clientId;
     private ActivityTypeVO type;
     private String target;
+
     @JsonIgnore
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
     private LocalDateTime createdAt;
+
+    @JsonProperty
+    public long getId() {
+        return id;
+    }
+
+    @JsonIgnore
+    public void setId(final long id) {
+        this.id = id;
+    }
+
+    @JsonProperty
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    @JsonIgnore
+    public void setCreatedAt(final LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
--- helpdesk-service/src/main/java/hu/schonherz/training/helpdesk/service/TestServiceImpl.java
+++
@@ -1,6 +0,0 @@
-package hu.schonherz.training.helpdesk.service;
-
-import hu.schonherz.training.helpdesk.service.api.TestService;
-
-public class TestServiceImpl implements TestService {
-}
