fd280bb6c0c08c27c7c40354d7d0cb695729b368
aron9609 <aron9609@gmail.com>
2017-02-06 19:59:23 +0100

    #33 slim scrollbar works fine, and chat.js is well-edited.

--- helpdesk-web/src/main/webapp/resources/js/chat.js
+++ helpdesk-web/src/main/webapp/resources/js/chat.js
@@ -1,17 +1,27 @@
 $(document).ready(function () {
     $("textarea").keyup(function (event) {
-        if (event.keyCode == 13) {
-            $("form").submit();
-        }
+        ifEnterClear();
     });
-    $(function () {
-        $('#textBox').slimscroll({
-            height: '250px'
-        });
-    });
+    showScroll();
 });
 
+function ifEnterClear() {
+    if (event.keyCode == 13) {
+        $("form").submit();
+    }
+}
+
+function showScroll() {
+    $('#textBox').slimscroll({
+        height: '250px'
+    });
+}
 
 function clearText() {
     $('textarea').val("");
+}
+
+function onSubmissionComplete() {
+    showScroll();
+    clearText();
 }
