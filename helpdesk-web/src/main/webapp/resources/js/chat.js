525e775a5e89a3a09ce6292fb92955caa3300d8d
aron9609 <aron9609@gmail.com>
2017-02-06 19:37:00 +0100

    #33 slim scrollbar works, but not completely

--- helpdesk-web/src/main/webapp/resources/js/chat.js
+++ helpdesk-web/src/main/webapp/resources/js/chat.js
@@ -1,8 +1,13 @@
-$(document).ready(function(){
-    $("textarea").keyup(function(event){
-        if(event.keyCode == 13){
+$(document).ready(function () {
+    $("textarea").keyup(function (event) {
+        if (event.keyCode == 13) {
             $("form").submit();
         }
+    });
+    $(function () {
+        $('#textBox').slimscroll({
+            height: '250px'
+        });
     });
 });
 
