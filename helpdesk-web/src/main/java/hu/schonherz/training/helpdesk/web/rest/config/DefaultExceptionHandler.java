aa8a29599c6d766e136873ec3e9b227130300d25
dratech <dratech92@gmail.com>
2017-02-14 17:40:23 +0100

    #45 Responding with json now.

--- helpdesk-web/src/main/java/hu/schonherz/training/helpdesk/web/rest/config/DefaultExceptionHandler.java
+++ helpdesk-web/src/main/java/hu/schonherz/training/helpdesk/web/rest/config/DefaultExceptionHandler.java
@@ -2,18 +2,21 @@ package hu.schonherz.training.helpdesk.web.rest.config;
 
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@Provider
 public class DefaultExceptionHandler implements ExceptionMapper<WebApplicationException>{
     private static Logger logger = LoggerFactory.getLogger(DefaultExceptionHandler.class);
 
     @Override
     public Response toResponse(WebApplicationException e) {
         logger.error(e.getMessage());
-        return null;
+        return Response.status(Status.BAD_REQUEST).entity(e.getMessage()).type("application/json").build(); 
     }
 
 }
