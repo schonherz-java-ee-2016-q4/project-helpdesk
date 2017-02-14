4fbf1daa7e447d08702d687555c6ac2b7280d148
dratech <dratech92@gmail.com>
2017-02-14 19:21:58 +0100

    #45 Registered the DefaultExceptionHandler

---
+++ helpdesk-data/src/main/java/hu/schonherz/training/helpdesk/data/main/Main.java
@@ -0,0 +1,14 @@
+package hu.schonherz.training.helpdesk.data.main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.error("sajt");
+    }
+
+}
--- helpdesk-web/src/main/java/hu/schonherz/training/helpdesk/web/rest/config/DefaultExceptionHandler.java
+++ helpdesk-web/src/main/java/hu/schonherz/training/helpdesk/web/rest/config/DefaultExceptionHandler.java
@@ -2,21 +2,21 @@ package hu.schonherz.training.helpdesk.web.rest.config;
 
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 @Provider
 public class DefaultExceptionHandler implements ExceptionMapper<WebApplicationException>{
     private static Logger logger = LoggerFactory.getLogger(DefaultExceptionHandler.class);
 
     @Override
-    public Response toResponse(WebApplicationException e) {
-        logger.error(e.getMessage());
-        return Response.status(Status.BAD_REQUEST).entity(e.getMessage()).type("application/json").build(); 
+    public Response toResponse(final WebApplicationException e) {
+        logger.error(e.getLocalizedMessage());
+        return Response.status(e.getResponse().getStatus()).entity(e.getMessage()).type("application/json").build(); 
     }
 
 }
