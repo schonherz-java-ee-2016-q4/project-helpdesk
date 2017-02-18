89fd8cc29eb64167510a13b41b24dff63a9fe8b0
Pintér Dávid <djazz90@gmail.com>
2017-02-18 14:38:15 +0100

    #52 Updated client activity page with color coded rows. New postman tests added.

--- helpdesk-service-api/src/main/java/hu/schonherz/training/helpdesk/service/api/vo/ActivityTypeVO.java
+++ helpdesk-service-api/src/main/java/hu/schonherz/training/helpdesk/service/api/vo/ActivityTypeVO.java
@@ -1,8 +1,18 @@
 package hu.schonherz.training.helpdesk.service.api.vo;
 
 public enum ActivityTypeVO {
-    BUTTON_CLICK,
-    NAVIGATION,
-    INPUT_FOCUSLOSS,
-    FORM_SUBMIT
+    BUTTON_CLICK("buttonClick"),
+    NAVIGATION("navigation"),
+    INPUT_FOCUSLOSS("inputFocusloss"),
+    FORM_SUBMIT("formSubmit");
+
+    private final String text;
+
+    ActivityTypeVO(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return this.text;
+    }
 }
