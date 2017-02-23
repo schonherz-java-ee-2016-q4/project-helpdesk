f48f1860d88bf072291c4b23f0c55b5d2b4610e5
Marcell Nemeth <marcellnemeth20@gmail.com>
2017-02-23 22:46:13 +0100

    #55 Created a 3 javascript method, with this methods the tab won't fade out when we click the right or left arrow

--- helpdesk-web/src/main/webapp/resources/js/statistics.js
+++ helpdesk-web/src/main/webapp/resources/js/statistics.js
@@ -1,5 +1,18 @@
-
 function addFocusMenu1(){
-    $('#lista1').addClass("active");
+    $('#homeTab').removeClass("active");
+    $('#home').removeClass("in active");
+    $('#tab1').addClass("active");
     $('#menu1').addClass("in active");
+}
+function addFocusMenu2(){
+    $('#homeTab').removeClass("active");
+    $('#home').removeClass("in active");
+    $('#tab2').addClass("active");
+    $('#menu2').addClass("in active");
+}
+function addFocusMenu3(){
+    $('#homeTab').removeClass("active");
+    $('#home').removeClass("in active");
+    $('#tab3').addClass("active");
+    $('#menu3').addClass("in active");
 }
