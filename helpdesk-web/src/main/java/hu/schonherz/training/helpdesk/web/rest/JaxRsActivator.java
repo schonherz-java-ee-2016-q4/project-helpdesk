694ffb76afb87a7cc6f4bf237f3d0003aa2a341c
aron9609 <aron9609@gmail.com>
2017-02-26 17:39:24 +0100

    #61 try rest service in prod

---
+++ helpdesk-web/src/main/java/hu/schonherz/training/helpdesk/web/rest/JaxRsActivator.java
@@ -0,0 +1,9 @@
+package hu.schonherz.training.helpdesk.web.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class JaxRsActivator extends Application {
+
+}
