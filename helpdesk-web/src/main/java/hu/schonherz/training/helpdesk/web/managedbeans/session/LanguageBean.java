36b59379a38a1b5314bd3352533f367df68d50e7
aron9609 <aron9609@gmail.com>
2017-02-26 13:47:27 +0100

    #61 modified language bean

--- helpdesk-web/src/main/java/hu/schonherz/training/helpdesk/web/managedbeans/session/LanguageBean.java
+++ helpdesk-web/src/main/java/hu/schonherz/training/helpdesk/web/managedbeans/session/LanguageBean.java
@@ -5,10 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -31,13 +28,6 @@ public class LanguageBean {
             locale = new Locale(language);
             updateLocaleMessages();
             FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
-            ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
-            log.info("language chaner invoked");
-            try {
-                externalContext.redirect(((HttpServletRequest) externalContext.getRequest()).getRequestURI());
-            } catch (IOException e) {
-                log.error("Failed to change language.", e);
-            }
         }
     }
 
